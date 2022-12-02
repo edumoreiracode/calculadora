9a93fed296aec3d5c9dfc2466a26366a3d04df78
Eduardo Moreira <eduardomoreiradev@gmail.com>
2022-12-01 21:14:06 -0300

    cambios en el display

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,20 @@
 
 let pantalla = document.getElementById("pantalla")
-let pantalla2 = document.getElementById("pantalla2")
-
-pantalla2.textContent = "0"
+pantalla.textContent = ""
 
 escribirEnPantalla = (valor)=>{
   pantalla.textContent += valor
 }
 
 mostrarResultado = ()=>{
-  pantalla2.textContent = eval(pantalla.textContent)
+  pantalla.textContent = eval(pantalla.textContent)
 }
 
 deletePantalla = ()=>{
   pantalla.textContent = ""
-  pantalla2.textContent = "0"
 }
 
 deleteUltimo = ()=>{
   cadena = pantalla.textContent
   pantalla.textContent = cadena.substring(0, cadena.length - 1);
-  if(pantalla.textContent == "") {pantalla2.textContent = "0"}
 }
